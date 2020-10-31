66d6e71c8f04bf1440401662854719f62caae2e0
BootyAss <panda_canniball@mail.ru>
2020-11-01 00:37:48 +0300

    interface fix

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ function f() {
 }
 
 window.onload = function () {
-    let preloader = document.getElementById('preloader');
     let content = document.getElementById('content');
     content.style.display = 'none';
+    let preloader = document.getElementById('preloader');
     setTimeout(f, 750);
 }
